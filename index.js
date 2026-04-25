0b9ef0e346e7e7a78ea6190b34635e26ce7840ca
sbrsbr772 <a01660035@tec.mx>
2026-04-24 20:41:23 -0600

    sobrescribiendo repo

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express"
 import morgan from "morgan"
 import mongoose from "mongoose";
 import indexRoute from "./routes/index.routes.js"
+import cors from "cors"
 import dotenv from "dotenv";
 
 //Configuración de base de datos
@@ -13,6 +14,14 @@ mongoose.connect(process.env.MONGODB_URI)
 
 //Configuración de express, puerto, y el resto de la app. 
 const app = express()
+
+//Configuración de CORS
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}))
 
 app.use(express.json());
 app.use(morgan("dev")) // sirve para ver el registro de los endpoints a los que has intentado acceder
